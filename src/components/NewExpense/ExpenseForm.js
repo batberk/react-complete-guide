a72aefa472ae5da5f113c175c06e30a9a07ccb6a
Muhammed Batuhan Berk <batuhan.berk@metu.edu.tr>
2022-06-02 15:33:56 +0300

    ExpenseForm reading inputs and logging to console

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,23 +1,32 @@
 import React from "react";
 import "./ExpenseForm.css";
 
+const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+}
+const amountChangeHandler = (event) => {
+    console.log(event.target.value);
+}
+const dateChangeHandler = (event) => {
+    console.log(event.target.value);
+}
 const ExpenseForm = () => {
   return (
     <form>
       <div className="expense-form__controls">
         <div className="expense-form__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler}/>
         </div>
 
         <div className="expense-form__control">
           <label>Amount</label>
-          <input type="number" min="0" step="0.01" />
+          <input type="number" min="0" step="0.01" onChange={amountChangeHandler}/>
         </div>
 
         <div className="expense-form__control">
           <label>Date</label>
-          <input type="date" min="2020-01-01" max="2022-12-31" />
+          <input type="date" min="2020-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className="new-expense__actions">
