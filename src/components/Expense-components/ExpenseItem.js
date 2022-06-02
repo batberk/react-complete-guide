ef53de635cc97870c7da7cdd7fa3a7335b8d3a95
Muhammed Batuhan Berk <batuhan.berk@metu.edu.tr>
2022-06-02 12:05:18 +0300

    button click update varying 2 strings

--- src/components/Expense-components/ExpenseItem.js
+++ src/components/Expense-components/ExpenseItem.js
@@ -1,17 +1,24 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  //
+  const [title, setTitle] = useState(props.title);
+
+  let amount = props.amount;
+  let date = props.date;
   const clickHandler = () => {
+    setTitle( title==="Updated" ?  "Updated2": "Updated");
     console.log("clicked");
   };
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title and Amount</button>
     </Card>
