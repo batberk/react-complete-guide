f3a9781d4a582b9235bd6bd06459a2620a440e6f
Muhammed Batuhan Berk <batuhan.berk@metu.edu.tr>
2022-06-09 16:56:39 +0300

    added some comments

--- src/components/Expense-components/Expenses.js
+++ src/components/Expense-components/Expenses.js
@@ -5,12 +5,15 @@ import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 
 function Expenses(props) {
+  //state for the filter
   const [getFilter, setGetFilter] = useState("2021");
+
+  //function to lift the filter state in ExpensesFilter to the Expenses component
   const filterChangeHandlerLiftUp = (filter) => {
     setGetFilter(filter);
     console.log(filter);
   };
-  {
+  
     /* {props.arr.map((expense) => (
       <ExpenseItem
         key={expense.id}
@@ -19,13 +22,16 @@ function Expenses(props) {
         date={expense.date}
       />
     ))} */
-  }
+
   // TODO print newly added expences passed somehow.
   //printing should be done dynamically fitting to the array size.
   return (
     <div className="expensesFilter">
       <Card className="expenses">
-        <ExpensesFilter selected={getFilter} onFilterChange={filterChangeHandlerLiftUp} />
+        <ExpensesFilter
+          selected={getFilter}
+          onFilterChange={filterChangeHandlerLiftUp}
+        />
         <ExpenseItem
           title={props.arr[0].title}
           amount={props.arr[0].amount}
@@ -51,7 +57,6 @@ function Expenses(props) {
           amount={props.arr[4].amount}
           date={props.arr[4].date}
         />
-        <ExpensesFilter />
       </Card>
     </div>
   );
