c31db5480961bb1468eeb2a1ab08442e357e3461
Muhammed Batuhan Berk <batuhan.berk@metu.edu.tr>
2022-06-16 16:46:02 +0300

    Add New Expense Button added

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,16 +3,33 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 //const NewExpense = (props) => {
 const NewExpense = (props) => {
-const submitNewExpenseHandler = (addedExpense) => {
-  const newExpense = {
-    ...addedExpense,
-    id: Math.random().toString(),
+  const [foröOpen, setFormOpen] = React.useState(false);
+
+  const formOpenHandlerTrue = () => {
+    setFormOpen(true);
   };
-  props.onNewExpenceAdd(newExpense);
-}
+
+  const formOpenHandlerFalse = () => {
+    setFormOpen(false);
+  };
+
+  const submitNewExpenseHandler = (addedExpense) => {
+    const newExpense = {
+      ...addedExpense,
+      id: Math.random().toString(),
+    };
+    props.onNewExpenceAdd(newExpense);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm onSubmitNewExpense={submitNewExpenseHandler}/>
+      {foröOpen ? (
+        <ExpenseForm
+          onSubmitNewExpense={submitNewExpenseHandler}
+          onFormClose={formOpenHandlerFalse}
+        />
+      ) : (
+        <button onClick={formOpenHandlerTrue}>Add new expense</button>
+      )}
     </div>
   );
 };
